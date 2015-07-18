80d80ba4545d9263d3e134c2c7ea5204bcdd879e
arnldlm <arnoldlam1@gmail.com>
2015-07-18 16:01:25 -0700

    is_associate fix

--- dashboard/views.py
+++ dashboard/views.py
@@ -377,15 +377,15 @@ def PublicProfileView(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
 	associates = request.user.user_profile.associates.all()
 
-	# is_associate = 0
-	# if user in associates:
-	# 	is_associate = 1 
+	is_associate = 0
+	if user.user_profile in associates:
+		is_associate = 1 
 
-	is_associate = request.user.user_profile.is_associate
+	is_associate = request.user.user_profile.is_associate(user)
 
 	return render(request, 'dashboard/public_profile.html', {
 		'user':user,
-		'is_associate':is_associate
+		'is_associate':is_associate,
 	})
 
 

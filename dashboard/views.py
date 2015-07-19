8bc5d8a1f442f8591752aad984fc9abf587a65e1
arnldlm <arnoldlam1@gmail.com>
2015-07-19 15:46:00 -0700

    note detail tweaks

--- dashboard/views.py
+++ dashboard/views.py
@@ -189,9 +189,9 @@ def AddNoteView(request):
 						doctor=doctor, medication_dose=medication_dose, next_dose=next_dose, weight=weight,
 						selfcare_instructions=selfcare_instructions, emergency_instructions=emergency_instructions)		
 			
+			# Optional parameters to be added to new_note object
 			if 'url' in form.cleaned_data:
 				new_note.url = form.cleaned_data['url']
-
 			if 'follow_up' in form.cleaned_data:
 				new_note.follow_up = form.cleaned_data['follow_up']
 

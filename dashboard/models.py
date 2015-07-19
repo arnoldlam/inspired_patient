e9d471c5e295c183904c304613e868ad2f9ca9a9
arnldlm <arnoldlam1@gmail.com>
2015-07-19 15:31:28 -0700

    hot fix

--- dashboard/models.py
+++ dashboard/models.py
@@ -105,7 +105,7 @@ class DischargeNote(Note):
 	weight = models.IntegerField()
 	medication_name = models.CharField(max_length=100)
 	medication_dose = models.CharField(max_length=100)
-	next_dose = models.DateTimeField()
+	next_dose = models.CharField()
 	selfcare_instructions = models.TextField()
 	emergency_instructions = models.TextField()
 	# attachments needs to be added

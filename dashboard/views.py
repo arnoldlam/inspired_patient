1d1ffee69411e0d61cad4f081b7818fee7acd56f
arnldlm <arnoldlam1@gmail.com>
2015-07-20 23:59:13 -0700

    added new note types to note creation from notebook

--- dashboard/views.py
+++ dashboard/views.py
@@ -122,7 +122,7 @@ def ClinicView(request):
 def NotesView(request):
 	user = request.user
 	notes = user.notes.all()
-	notes = notes.filter(date_accessed__lte=timezone.now()).order_by('-date_accessed')[:5]
+	notes = notes.filter(date_accessed__lte=timezone.now()).order_by('-date_accessed')[:10]
 	notebooks_read_only = user.notebooks_read_only.all()
 	notebooks_read_write = user.notebooks_read_write.all()
 

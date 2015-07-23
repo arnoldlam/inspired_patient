3ce4d5826c0f1b242be2242f3d9050c885d521d4
arnldlm <arnoldlam1@gmail.com>
2015-07-22 23:44:57 -0700

    hot fix

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,9 +5,8 @@ from dashboard.models import UserProfile, DischargeNote, Notebook
 from django.contrib.auth.models import User, Group
 
 class AddNoteForm(forms.Form):
-	def __init__(self, test_values, *args, **kwargs):
+	def __init__(self, test_values):
 		self.USER_CHOICES = test_values
-		super(AddNoteForm, self).__init__(*args, **kwargs)
 		self.fields['choices'] = forms.ModelMultipleChoiceField(widget=CheckboxSelectMultiple, choices = self.USER_CHOICES)
 
 	subject = forms.CharField(label='Subject', max_length=150)

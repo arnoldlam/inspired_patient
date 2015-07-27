ed1f87c0fc551dd5c76f49d9014bdee7963fc3b6
arnldlm <arnoldlam1@gmail.com>
2015-07-26 18:42:15 -0700

    added views and template for user creation

--- inspired_health_site/urls.py
+++ inspired_health_site/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from dashboard import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^dashboard/', include('dashboard.urls', namespace="dashboard")),
     url('^accounts/', include('django.contrib.auth.urls')),
+    url('^accounts/sign_up', views.CreateNewUserView, name='create_user'),
 ]

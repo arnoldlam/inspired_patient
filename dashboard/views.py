5ce8f071c7da20ae3b22a2ae17228043e412269c
arnldlm <arnoldlam1@gmail.com>
2015-07-28 23:53:20 -0700

    added automatic login after profile creation

--- dashboard/views.py
+++ dashboard/views.py
@@ -50,10 +50,11 @@ def CreateNewUserView(request):
 				return render(request, 'dashboard/create_user.html', {
 					'form':form,
 			})
-			login(request, new_user)
-			return render(request, 'dashboard/index.html', {
-				'user':new_user,
-			})
+	# Automatically log a user in after user creation
+	login(request, new_user)
+	return render(request, 'dashboard/index.html', {
+		'user':new_user,
+	})
 	else:
 		# Allow user to select a role
 		user_form = UserCreationForm(prefix='user_form')

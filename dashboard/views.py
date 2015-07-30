ef7d2905d5d536297cc84fadd2e167df5e47e8b0
arnldlm <arnoldlam1@gmail.com>
2015-07-29 21:39:50 -0700

    added addition profile creation for professionals

--- dashboard/views.py
+++ dashboard/views.py
@@ -45,19 +45,18 @@ def CreateNewUserView(request):
 					title=title,
 				)
 				new_user_profile.save()
-			
-				if 'is_professional' not in user_profile_form.cleaned_data:
-					username = user_form.cleaned_data['username']
-					password = user_form.cleaned_data['password1']
-					user = authenticate(username=username, password=password)
-					login(request, user)
-					return HttpResponseRedirect('/dashboard/')
-					
+
 				if user_profile_form.cleaned_data['is_professional'] == True:
 					form = CreateProfessionalProfileForm()
 					return render(request, 'dashboard/create_professional.html', {
 						'form':form,
 					})
+
+				username = user_form.cleaned_data['username']
+				password = user_form.cleaned_data['password1']
+				user = authenticate(username=username, password=password)
+				login(request, user)
+				return HttpResponseRedirect('/dashboard/')
 	else:
 		# Allow user to select a role
 		user_form = UserCreationForm(prefix='user_form')

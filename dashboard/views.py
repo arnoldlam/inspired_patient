2fae6dc847a2bf6e48d69517105e28cf7b018200
arnldlm <arnoldlam1@gmail.com>
2015-07-29 19:42:24 -0700

    auto login after account creation

--- dashboard/views.py
+++ dashboard/views.py
@@ -45,11 +45,7 @@ def CreateNewUserView(request):
 			)
 			new_user_profile.save()
 
-			user = authenticate(username=user_form.cleaned_data['username'], 
-				password=user_form.cleaned_data['password2'])
-			login(request, user)
-			return render(request, 'dashboard/index.html', {
-				'user':user,
+			return render(request, 'accounts/login', {
 			})
 
 			if user_profile_form.cleaned_data['is_professional'] == True:

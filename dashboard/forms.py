689c9b2f08a7dc6e762557989efa8b226195a3c9
arnldlm <arnoldlam1@gmail.com>
2015-08-03 16:08:24 -0700

    added intial time to forms

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -66,7 +66,7 @@ class AddProcedureNoteForm(NotesThatRelateToDoctorAndClinic):
 		widget=forms.Textarea)
 
 class AddSelfCareNoteForm(AddNoteForm):
-	date_and_time = forms.DateField('Data/Time', initial=timezone.now())
+	date_and_time = forms.DateField('Data/Time', initial=datetime.datetime.now())
 	description = forms.CharField(label='Description', max_length=4000, widget=forms.Textarea)
 	frequency = forms.CharField(label='Frequency', max_length=150)
 	procedure = forms.CharField(label='Procedure', max_length=500, widget=forms.Textarea)
@@ -78,7 +78,7 @@ class AddResourceNoteForm(NotesThatRelateToDoctorAndClinic):
 		super(AddResourceNoteForm, self).__init__(user_id, *args, **kwargs)
 
 class AddAppointmentNoteForm(NotesThatRelateToDoctorAndClinic):
-	date_and_time = forms.DateField('Appointment Data/Time', initial=timezone.now())
+	date_and_time = forms.DateField('Appointment Data/Time', initial=datetime.datetime.now())
 	reason_for_visit = forms.CharField(label="Reason for Visit", max_length=200)
 
 class AddContactNoteForm(AddNoteForm):
@@ -110,7 +110,7 @@ class AddMedicationNoteForm(AddNoteForm):
 		('UK', 'United Kingdom'),
 	)
 
-	date_and_time = forms.DateField('Data/Time', initial=timezone.now())
+	date_and_time = forms.DateField('Data/Time', initial=datetime.datetime.now())
 	medication_name = forms.CharField(label='Medication Name', max_length=100)
 	medication_dosage = forms.CharField(label='Medication Dosage', max_length=100)
 	medication_frequency = forms.CharField(label='Medication Frequency', max_length=100)

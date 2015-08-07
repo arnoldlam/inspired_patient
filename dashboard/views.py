b5a2cba978ab1fce2cfad7e550da0361d2281427
arnldlm <arnoldlam1@gmail.com>
2015-08-06 22:01:17 -0700

    recurring notes

--- dashboard/views.py
+++ dashboard/views.py
@@ -553,12 +553,12 @@ def AddSelfCareNoteView(request):
 			end_date = form.cleaned_data['end_date']
 			if (end_date == 'every_day'):
 				recurring_date = date_and_time
-				while (recurring_date < end_date):
-					recurring_date = recurring_date + datetime.timedelta(days=1)
-					new_note.pk = None
-					new_note.id = None
-					new_note.save()
-					new_note.date_and_time = recurring_date
+				# while (recurring_date < end_date):
+				recurring_date = recurring_date + datetime.timedelta(days=1)
+				new_note.pk = None
+				new_note.id = None
+				new_note.save()
+				new_note.date_and_time = recurring_date
 
 			# URL for redirect to newly created note's detail page
 			redirect_url = reverse('dashboard:note_detail', kwargs={'note_id': new_note.id})

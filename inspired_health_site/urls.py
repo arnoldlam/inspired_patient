e765e22b70fbd3e72922e7191e0f77a5685f74c5
arnldlm <arnoldlam1@gmail.com>
2015-08-07 22:15:57 -0700

    moved professional creation to seperate view

--- inspired_health_site/urls.py
+++ inspired_health_site/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     url(r'^dashboard/', include('dashboard.urls', namespace="dashboard")),
     url('^accounts/', include('django.contrib.auth.urls')),
     url('^accounts/sign_up', views.CreateNewUserView, name='create_user'),
-    url('^accounts/sign_up/professional', views.CreateNewProfessionalView, name='create_professional'),
+    url('^accounts/sign_up_professional', views.CreateNewProfessionalView, name='create_professional'),
 ]

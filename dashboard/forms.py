3e09d17ab3efa9dc942b999b8920e5805eb5224c
arnldlm <arnoldlam1@gmail.com>
2015-08-10 22:24:01 -0700

    hot fix

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -256,8 +256,8 @@ class UserProfileCreationForm(forms.Form):
 	profile_picture = forms.ImageField(required=False)
 	title = forms.CharField(widget=Select(choices=TITLE_SELECT))
 	phone_number = forms.CharField(label="Phone Number", initial="123-123-1234", widget=forms.TextInput(attrs={'class':'form-control'}))
-	medical_history = forms.CharField(label='Medical History', max_length=4000, widget=forms.Textarea,
-	 initial="None", widget=forms.TextInput(attrs={'class':'form-control', 'rows':'6'}))
+	medical_history = forms.CharField(label='Medical History', max_length=4000, initial="None", 
+		widget=forms.TextInput(attrs={'class':'form-control', 'rows':'6'}))
 
 	address_unit = forms.CharField(label='Unit', max_length=10, initial="123", widget=forms.TextInput(attrs={'class':'form-control'}))
 	address_street = forms.CharField(label='Street', max_length=50, initial="Memory Lane", widget=forms.TextInput(attrs={'class':'form-control'}))

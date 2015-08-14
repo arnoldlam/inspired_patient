17969a7c99d11653ba85953844355573202ffa4d
arnldlm <arnoldlam1@gmail.com>
2015-08-13 22:38:10 -0700

    added notebook view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1301,13 +1301,20 @@ def SchedulingView(request):
 
 	# Get all appointment, medication, and self care notes that belong to user and sort by date
 	upcoming_tasks = Note.objects.filter(Q(editors__id=user.id) | Q(viewers__id=user.id) | Q(author__id=user.id))
-	upcoming_tasks = upcoming_tasks.filter(Q(note_type__exact='appointment_note') | Q(note_type__exact='medication_note') | Q(note_type__exact='self_care_note'))
-	upcoming_tasks = upcoming_tasks.filter(date_and_time__gte=timezone.now()).order_by('date_and_time')[:10]
-
+	
+	appointments = upcoming_tasks.filter(note_type__exact='appointment_note')
+	appointments = appointments.filter(date_and_time__gte=timezone.now()).order_by('date_and_time')[:10]
+	self_care_notes = upcoming_tasks.filter(note_type__exact='self_care_note')
+	self_care_notes = self_care_notes.filter(date_and_time__gte=timezone.now()).order_by('date_and_time')[:10]
+	medication_notes = upcoming_tasks.filter(note_type__exact='medication_note')
+	medication_notes = medication_notes.filter(date_and_time__gte=timezone.now()).order_by('date_and_time')[:10]
+	
 	patient_appointments = user.appointments
 
-	return render(request, 'dashboard/scheduling.html', {
-		'upcoming_tasks':upcoming_tasks,
+	return render(request, 'dashboard/calendar.html', {
+		'appointments':appointments,
+		'self_care_notes':self_care_notes,
+		'medication_notes':medication_notes,
 		'user':user, 	
 		'patient_appointments':patient_appointments,
 	})

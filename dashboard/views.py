0923faa5a24277900dc58a503bd7eafdbc8a9c86
arnldlm <arnoldlam1@gmail.com>
2015-08-22 22:09:04 -0700

    added notification when communication note is created

--- dashboard/views.py
+++ dashboard/views.py
@@ -545,7 +545,7 @@ def AddCommunicationNoteView(request):
 			# Add note to recipient's notes
 			team_member.notes_read_write.add(new_note)
 			message = request.user.user_profile.full_name() + " sent the message '" + new_note.subject + "' to you."
-			redirect_url = reverse('dashboard:note_detail', kwargs={'new_note.id': note_id})
+			redirect_url = reverse('dashboard:note_detail', kwargs={'note_id': new_note.id})
 			action_url = redirect_url + "?note_type=" + new_note.note_type
 			notifcation = Notification(message=message, recipient=team_member, action_url=action_url)
 

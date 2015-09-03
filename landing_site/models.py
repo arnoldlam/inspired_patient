33f5f81b09aa7b16f253cc1b654f862e992ee485
arnldlm <arnoldlam1@gmail.com>
2015-09-03 15:03:57 -0700

    added contact us page

--- landing_site/models.py
+++ landing_site/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.CharField(max_length=4000)
+	date_created = models.DateTimeField('date created', auto_now_add=True)
+	author_name = models.CharField(max_length = 100)

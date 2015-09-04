089f77a3de38afc3a3c70189f036f89dda84c752
arnldlm <arnoldlam1@gmail.com>
2015-09-04 16:43:33 -0700

    bug tracker fixes

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -124,18 +124,18 @@ class RepeatingNotesForm(AddNoteForm):
 		self.DAY_CHOICES = zip(self.day_list, self.day_list)
 
 		self.MONTH_CHOICES = (
-			('01', 'January'),
-			('02', 'February'),
-			('03', 'March'),
-			('04', 'April'),
+			('01', 'Jan'),
+			('02', 'Feb'),
+			('03', 'Mar'),
+			('04', 'Apr'),
 			('05', 'May'),
-			('06', 'June'),
-			('07', 'July'),
-			('08', 'August'),
-			('09', 'September'),
-			('10', 'October'),
-			('11', 'November'),
-			('12', 'December'),
+			('06', 'Jun'),
+			('07', 'Jul'),
+			('08', 'Aug'),
+			('09', 'Sept'),
+			('10', 'Oct'),
+			('11', 'Nov'),
+			('12', 'Dec'),
 		)
 
 		self.YEAR_CHOICES = (
